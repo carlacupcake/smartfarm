792f9c69f51827c5b38f65a4d6572163678964b9
Carla Becker <carla.joy.becker@gmail.com>
2024-08-13 13:41:19 -0700

    results in python and matlab match

--- code/total_model_param_fitting.m
+++ code/total_model_param_fitting.m
@@ -100,6 +100,7 @@ P0 = 0.01;
 h = h0*ones(N, 1);
 A = A0*ones(N, 1);
 c = c0*ones(N, 1);
+DADT = zeros(N, 1);
 P = P0*ones(N, 1);
 
 % Plant Height Parameters
@@ -115,38 +116,42 @@ khf = 1;
 Hf = 0.01; 
 
 % Leaf Area Parameters
-kL = 15e-4; % leaf carrying capacity m2
+hthreshold_leaf = 1/4 * kh;
+kL = 15e-4; 
 bLT = 65; 
 cLT = 20; 
-LT = 5e-5; % goal is 15 cm2 per 30 days or 5e-5 m2 per day
-aLT = 25e-4; 
-dLT = 5e-4;
+LT = 2.5e-6; 
+aLT = 8e-2; 
+dLT = 1e-3;
 bLW = 37; 
 cLW = 20; 
-Lw = 1e-2;
-aLw = 8e-4;
-dLW = 1e-4; 
+Lw = 6e-3; 
+aLw = 8e-2;
+dLW = 8e-4; 
 kLF = 0.7;
 Lf = 1e-3;
-aLf = 1e-4; 
+aLf = 8e-4; 
 
 % Canopy Biomass Parameters
+dAdt_threshold_fruit_decay = -2e-6;
+ac = 5e-2;
 ce = 0.05;
 R0 = 2045;
 kappa = sin(pi/8);
 rhostd = 1;
-kc = 1;
-dc = 5e-3;
+kc = 1; 
+dc = 1e-2;
 
 % Fruit Biomass Parameters
-cthreshold_fruit = 3/4 * kc;
-kP = 5; % kg
-ap = 50; 
-dp = 0.001;
+cthreshold_fruit = 1/4 * kc;
+kP = 5; 
+ap = 5e-2; 
+dp = 1e-2;
 
 % Simulation
 height_decay_triggered = false;
 leaf_decay_triggered = false;
+fruit_canopy_decay_triggered = false;
 for n=1:N-1
 
     % Plant Height Update
@@ -169,60 +174,72 @@ for n=1:N-1
     if A(n) > kL
         leaf_decay_triggered = true;
     end
-    if leaf_decay_triggered
-        dAdt = LT * aLT * Teff(n) * exp(-((Tc(n) - bLT*Teff(n))/(cLT*Teff(n)))^2)...
-               + Lw * aLw * Weff(n) * exp(-((Wc(n) - bLW*Weff(n))/(cLW*Weff(n)))^2)...
-               + Lf * aLf * Feff(n) * (1 - Feff(n)/kLF)...
-               - dLT * TSL(n) * A(n) - dLW * WSL(n) * A(n);
+    if h(n) > hthreshold_leaf
+        if leaf_decay_triggered
+            dAdt = LT * aLT * Teff(n) * exp(-((Tc(n) - bLT*Teff(n))/(cLT*Teff(n)))^2)...
+                   + Lw * aLw * Weff(n) * exp(-((Wc(n) - bLW*Weff(n))/(cLW*Weff(n)))^2)...
+                   + Lf * aLf * Feff(n) * (1 - Feff(n)/kLF)...
+                   - dLT * TSL(n) * A(n) - dLW * WSL(n) * A(n);
+        else
+            dAdt = LT * aLT * Teff(n) * exp(-((Tc(n) - bLT*Teff(n))/(cLT*Teff(n)))^2)...
+                   + Lw * aLw * Weff(n) * exp(-((Wc(n) - bLW*Weff(n))/(cLW*Weff(n)))^2)...
+                   + Lf * aLf * Feff(n) * (1 - Feff(n)/kLF);
+        end
     else
-        dAdt = LT * aLT * Teff(n) * exp(-((Tc(n) - bLT*Teff(n))/(cLT*Teff(n)))^2)...
-               + Lw * aLw * Weff(n) * exp(-((Wc(n) - bLW*Weff(n))/(cLW*Weff(n)))^2)...
-               + Lf * aLf * Feff(n) * (1 - Feff(n)/kLF);
+        dAdt = 0;
     end
     A(n+1) = A(n) + dAdt * dt;
+    DADT(n+1) = dAdt;
 
     % Canopy Biomass Update
-    if leaf_decay_triggered
-        dcdt = ce * R0 * (1 - exp(-kappa/rhostd * 3 * c(n)/A(n) * sqrt(pi/A(n)) * tan(pi/4)))...
-               * A(n) * c(n) * (1 - c(n)/kc) - dc*c(n);
+    rho = 3 * c(n)/A(n) * sqrt(pi/A(n)) * tan(pi/4);
+    R = R0 * (1 - exp(-kappa * rho/rhostd));
+    if dAdt < dAdt_threshold_fruit_decay
+        fruit_canopy_decay_triggered = true;
+    end
+    if fruit_canopy_decay_triggered
+        dcdt = ce * R * A(n) * (ac*c(n) * (1 - c(n)/kc) - dc*c(n));
     else
-        dcdt = ce * R0 * (1 - exp(-kappa/rhostd * 3 * c(n)/A(n) * sqrt(pi/A(n)) * tan(pi/4)))...
-               * A(n) * c(n) * (1 - c(n)/kc);
+        dcdt = ce * R * A(n) * ac * c(n) * (1 - c(n)/kc);
     end    
     c(n+1) = c(n) + dcdt * dt;
 
     % Fruit Biomass Update
-    if P(n) >= kP
-        leaf_decay_triggered = true;
-    end
-    if leaf_decay_triggered
-        dPdt = ap * TSP(n) * dcdt * P(n) * (1 - P(n)/kP * kc/c(n))- dp*P(n);
-    elseif c(n) < cthreshold_fruit
+    if c(n) < cthreshold_fruit
         dPdt = 0;
     else
-        dPdt = ap * TSP(n) * dcdt * P(n) * (1 - P(n)/kP * kc/c(n));
-    end
+        if fruit_canopy_decay_triggered
+            dPdt = ap * TSP(n) * c(n)/kc * P(n) * (1 - P(n)/kP)- dp*P(n);
+        else
+            dPdt = ap * TSP(n) * c(n)/kc * P(n) * (1 - P(n)/kP);
+        end
+    end    
     P(n+1) = P(n) + dPdt * dt;
 
 end
 
 figure(4)
-subplot(4, 1, 1)
+subplot(5, 1, 1)
 plot(1:N, h)
 xlabel('Hour index')
 ylabel('Plant Height (m)')
 title('Components of Crop Growth')
-subplot(4, 1, 2)
+subplot(5, 1, 2)
 plot(1:N, A) 
 xlabel('Hour index')
 ylabel('Leaf Area (m2)')
-subplot(4, 1, 3)
+subplot(5, 1, 3)
+plot(1:N, DADT) 
+xlabel('Hour index')
+ylabel('dA/dt (m2/s)')
+subplot(5, 1, 4)
 plot(1:N, c) 
 xlabel('Hour index')
 ylabel('Canopy Biomass (kg)')
-subplot(4, 1, 4)
+subplot(5, 1, 5)
 plot(1:N, P) 
 xlabel('Hour index')
 ylabel('Fruit Biomass (kg)')
+set(gcf, 'Position', [100, 100, 600, 800]);
 
 end
